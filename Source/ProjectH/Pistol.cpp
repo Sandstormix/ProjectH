a4dbf5568eff06d12d209147c114052a25eedbc6
Dariusz-A-Piotrowski <64169694+Sandstormix@users.noreply.github.com>
2023-11-25 16:06:10 +0000

    Added child class `Pistol` of type `RangeWeapon`

---
+++ Source/ProjectH/Pistol.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pistol.h"
+
+APistol::APistol()
+{
+	
+}
+
+APistol::~APistol()
+{
+	
+}
+
+void APistol::OnWeaponUse()
+{
+	
+}
---
+++ Source/ProjectH/Pistol.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RangeWeapon.h"
+#include "Pistol.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTH_API APistol : public ARangeWeapon
+{
+	GENERATED_BODY()
+
+	APistol();
+	~APistol();
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+	void OnWeaponUse() override;
+	
+};
