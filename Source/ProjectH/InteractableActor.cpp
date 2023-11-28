ccfe51b418b8a390a1da8b835f12b8c0b37b4d2f
Dariusz-A-Piotrowski <64169694+Sandstormix@users.noreply.github.com>
2023-11-28 10:25:02 +0000

    Added `InteractableActor` as parent class for interactable objects

---
+++ Source/ProjectH/InteractableActor.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InteractableActor.h"
+
+// Sets default values
+AInteractableActor::AInteractableActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh Component"));
+	RootComponent = MeshComponent;
+	
+	BoxCollider = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Collider"));
+	BoxCollider->SetupAttachment(RootComponent);
+}
+
+void AInteractableActor::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	BoxCollider->OnComponentBeginOverlap.AddDynamic(this, &AInteractableActor::OnBoxBeginOverlap);
+	BoxCollider->OnComponentEndOverlap.AddDynamic(this, &AInteractableActor::OnBoxEndOverlap);
+}
+
+// Called when the game starts or when spawned
+void AInteractableActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void AInteractableActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AInteractableActor::OnInteract(bool bCanInteract)
+{
+	if(bCanInteract)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15, FColor::Yellow, TEXT("Interaction"));
+	}
+}
+
+void AInteractableActor::OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                           UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	
+}
+
+void AInteractableActor::OnBoxEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex)
+{
+	
+}
+
---
+++ Source/ProjectH/InteractableActor.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interactable.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/Actor.h"
+#include "InteractableActor.generated.h"
+
+class AProjectHCharacter;
+
+UCLASS()
+class PROJECTH_API AInteractableActor : public AActor, public IInteractable
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AInteractableActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	virtual void PostInitializeComponents() override;
+	virtual void OnInteract(bool bCanInteract) override;
+	
+public:
+	UFUNCTION()
+	virtual void OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent,
+		int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+	virtual void OnBoxEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent,
+		int32 OtherBodyIndex);
+
+protected:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* MeshComponent;
+	
+	UPROPERTY(VisibleAnywhere)
+	UBoxComponent* BoxCollider;
+
+	UPROPERTY(VisibleAnywhere)
+	AProjectHCharacter* PlayerReference;
+};
