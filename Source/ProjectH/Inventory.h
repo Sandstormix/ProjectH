0d50628768d30fe653ec655751ca91c3b47dfb74
Dariusz-A-Piotrowski <64169694+Sandstormix@users.noreply.github.com>
2024-02-27 13:41:06 +0000

    Created basic layout for Inventory and added display functionality for it

---
+++ Source/ProjectH/Inventory.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Inventory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTH_API UInventory : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	
+private:
+	
+};
